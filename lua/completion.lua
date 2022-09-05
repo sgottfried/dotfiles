4a9522a5d26ca56d17bc6efc9859ec2218270a89
Sam Gottfried <sgottfrd@gmail.com>
2022-09-05 14:22:20 -0400

    Create completion.lua

---
+++ lua/completion.lua
@@ -0,0 +1,30 @@
+-- Autocomplete --
+local cmp = require 'cmp'
+
+if(cmp ~= nil) then
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["UltiSnips#Anon"](args.body)
+      end,
+    },
+    mapping = {
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<C-y>'] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      },
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'ultisnips' },
+    }, {
+      { name = 'buffer' },
+    }),
+  })
+end
