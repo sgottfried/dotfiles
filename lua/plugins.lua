458708776d09e54c0a05d72387bf97dde7dee67c
Sam Gottfried <sgottfrd@gmail.com>
2023-05-31 15:07:46 -0400

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,44 +1,69 @@
 local packer = require('packer')
 
 packer.startup(function(use)
-  use 'tpope/vim-unimpaired'
-  use 'wbthomason/packer.nvim'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use 'SirVer/ultisnips'
+  use 'APZelos/blamer.nvim'
   use 'airblade/vim-gitgutter'
   use 'ellisonleao/gruvbox.nvim'
-  use 'honza/vim-snippets'
+  use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'ianks/vim-tsx'
-  use 'junegunn/fzf'
   use 'leafgarland/typescript-vim'
   use 'mxw/vim-jsx'
   use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/plenary.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }}
   use 'pangloss/vim-javascript'
-  use { 'quangnguyen30192/cmp-nvim-ultisnips', requires = {{ 'hrsh7th/nvim-cmp'}}}
+  use 'theHamsta/nvim-dap-virtual-text'
+  use 'tpope/vim-commentary'
   use 'tpope/vim-fugitive'
-  use 'tpope/vim-projectionist'
-  use 'tpope/vim-rhubarb'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-unimpaired'
   use 'tpope/vim-vinegar'
+  use 'wbthomason/packer.nvim'
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }}
   use { 'nvim-telescope/telescope.nvim', requires = {{ "nvim-telescope/telescope-live-grep-args.nvim" }},
     config = function()
       require('telescope').load_extension('live_grep_args')
     end
   }
-  use { 'nvim-lualine/lualine.nvim', requires = {{ 'kyazdani42/nvim-web-devicons', opt = true }}}
-  use { 'KadoBOT/nvim-spotify', run = 'make',
-  config = function()
-    local spotify = require'nvim-spotify'
-
-    spotify.setup {
-      -- default opts
-      status = {
-        update_interval = 10000, -- the interval (ms) to check for what's currently playing
-        format = '%s %t by %a' -- spotify-tui --format argument
-      }
-    }
-  end }
+  use { 'nvim-lualine/lualine.nvim', requires = {{ 'kyazdani42/nvim-web-devicons', opt = true }},
+    config = function()
+      require('lualine').setup({})
+    end
+  }
+  use {
+    "nvim-neorg/neorg",
+    run = ":Neorg sync-parsers",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+        require('neorg').setup {
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.integrations.treesitter"] = {},
+                ["core.concealer"] = {
+                    config = {
+                        icon_preset = "diamond",
+                        icons = {
+                            todo = {
+                                done = {
+                                    icon = '✓'
+                                },
+                                pending = {
+                                    icon = '□'
+                                }
+                            }
+                        }
+                    }
+                }, -- Adds pretty icons to your documents
+                ["core.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                            default_workspace = 'notes'
+                        },
+                    },
+                },
+            },
+        }
+    end
+}
 end)
