6a2f25078a2a071c76d09596379ab7688a41c117
Sam Gottfried <sgottfrd@gmail.com>
2024-02-07 09:29:20 -0500

    Update telescope-setup.lua

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -2,18 +2,18 @@ local telescope = require("telescope")
 local lga_actions = require("telescope-live-grep-args.actions")
 
 telescope.setup {
-  defaults = { file_ignore_patterns = {"node_modules"} },
-  extensions = {
-    live_grep_args = {
-      auto_quoting = true, -- enable/disable auto-quoting
-      -- override default mappings
-      -- default_mappings = {},
-      mappings = { -- extend mappings
-        i = {
-          ["<C-k>"] = lga_actions.quote_prompt({ postfix = [[ -g '!**/**test*' -i]] })
+    defaults = { file_ignore_patterns = { "node_modules" } },
+    extensions = {
+        live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- override default mappings
+            -- default_mappings = {},
+            mappings = { -- extend mappings
+                i = {
+                    ["<C-k>"] = lga_actions.quote_prompt({ postfix = [[ -g '!**/**test*' -i]] })
 
+                }
+            }
         }
-      }
     }
-  }
 }
