8840b665064624bea50b321f1a43e827d59155c9
Sam Gottfried <sgottfrd@gmail.com>
2024-03-12 18:13:33 -0400

    Update and rename Wezterm.lua to wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -28,5 +28,7 @@ return {
         },
         { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection('Down') },
         { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection('Up') },
+        { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
+        { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection('Right') },
     }
 }
