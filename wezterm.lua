ae78f82022fedab17b2327077869a6684c26267e
Sam Gottfried <sgottfrd@gmail.com>
2024-03-12 19:04:47 -0400

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,16 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
+local mux = wezterm.mux
 
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    local gui_window = window:gui_window();
+
+    gui_window:maximize()
+
+    pane:split { size = 0.6, direction = "Top" }
+    pane:split({ direction = "Right" })
+end)
 return {
     color_scheme = "Gruvbox dark, hard (base16)",
     font = wezterm.font("Fira Code"),
@@ -26,9 +36,13 @@ return {
             action = act.SplitPane({ direction = "Right" }),
             command = { args = { domain = "CurrentPaneDomain" } }
         },
+        { key = 'c', mods = 'LEADER', action = act.SpawnTab("CurrentPaneDomain") },
+        { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
         { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection('Down') },
         { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection('Up') },
-        { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
         { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection('Right') },
+        { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1) },
+        { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1) },
+        { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
     }
 }
