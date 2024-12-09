298afc974ebfae7835960f3c2a3003019545c170
Sam Gottfried <sgottfrd@gmail.com>
2024-12-09 09:25:33 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,95 +13,146 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require("lazy").setup({
     'APZelos/blamer.nvim',
+    'HiPhish/rainbow-delimiters.nvim',
     'airblade/vim-gitgutter',
     'ellisonleao/gruvbox.nvim',
     'github/copilot.vim',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/nvim-cmp',
     'ianks/vim-tsx',
-    'jose-elias-alvarez/null-ls.nvim',
     'leafgarland/typescript-vim',
-    'mxw/vim-jsx',
+    'mfussenegger/nvim-jdtls',
     'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
     'pangloss/vim-javascript',
-    'theHamsta/nvim-dap-virtual-text',
     'tpope/vim-commentary',
-    'tpope/vim-fugitive',
+    'tpope/vim-projectionist',
     'tpope/vim-surround',
     'tpope/vim-unimpaired',
-    'tpope/vim-vinegar',
-    { "rcarriga/nvim-dap-ui",            dependencies = { "mfussenegger/nvim-dap" } },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    { "mxsdev/nvim-dap-vscode-js",       dependencies = { "mfussenegger/nvim-dap" } },
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-neotest/neotest-jest"
+        },
+        config = function()
+            require('neotest').setup({
+                adapters = {
+                    require('neotest-jest')({
+                        cwd = function(path)
+                            return vim.fn.getcwd()
+                        end,
+                    }),
+                }
+            })
+        end
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",  -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        config = true
+    },
+    {
+        'stevearc/oil.nvim',
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- We'd like this plugin to load first out of the rest
+        config = true,   -- This automatically runs `require("luarocks-nvim").setup()`
+    },
+    {
+        "nvim-neorg/neorg",
+        dependencies = { "luarocks.nvim", "nvim-neorg/neorg-telescope" },
+        -- put any other flags you wanted to pass to lazy here!
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {},  -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.integrations.telescope"] = {},
+                    ["core.journal"] = {
+                        config = {
+                            workspace = "journal",
+                        }
+                    },
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            default_workspace = "journal",
+                            workspaces = {
+                                journal = "~/notes"
+
+                            },
+                        },
+                    },
+                    ["core.qol.todo_items"] = {},
+                    ["core.ui"] = {},
+                },
+            })
+        end,
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", },
     {
         'creativenull/efmls-configs-nvim',
         version = 'v1.x.x', -- version is optional, but recommended
         dependencies = { 'neovim/nvim-lspconfig' },
     },
-    {
-        "microsoft/vscode-js-debug",
-        opt = true,
-        run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
-    },
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.3',
-        dependencies = {
-            {
-                'nvim-lua/plenary.nvim',
-                "nvim-telescope/telescope-live-grep-args.nvim",
-                -- This will not install any breaking changes.
-                -- For major updates, this must be adjusted manually.
-                version = "^1.0.0",
-            },
-        },
-        config = function()
-            require("telescope").load_extension("live_grep_args")
-        end
-    },
-
+    { 'nvim-telescope/telescope.nvim',   tag = '0.1.3', },
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { { 'kyazdani42/nvim-web-devicons', opt = true } },
         config = function()
-            require('lualine').setup({})
+            require('lualine').setup({
+                options = {
+                    theme = 'gruvbox',
+                },
+                sections = {
+                    lualine_c = {
+                        "filename",
+                        function()
+                            return require("nvim-treesitter").statusline({
+                                indicator_size = 100,
+                                type_patterns = { "class", "function", "method", "pair" },
+                                separator = " -> ",
+                            })
+                        end,
+                    },
+                    lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } },
+                        "filetype" },
+                },
+            })
         end
     },
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        opts = {} -- this is equalent to setup({}) function
-    },
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        -- tag = "*",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("neorg").setup {
-                load = {
-                    ["core.defaults"] = {},  -- Loads default behaviour
-                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.dirman"] = {      -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/notes",
-                            },
-                        },
-                    },
-                    ["core.ui"] = {},
-                    -- ["core.tempus"] = {},
-                    -- ["core.ui.calendar"] = {}
-                },
-            }
-        end,
-    },
+    { 'windwp/nvim-autopairs', event = "InsertEnter", opts = {} },
 })
 
+
 -- Settings --
+vim.g.copilot_filetypes = { norg = false }
 vim.g.blamer_date_format = '%m/%d/%y'
 vim.g.blamer_enabled = 1
 vim.g.blamer_show_in_visual_modes = 0
@@ -113,8 +164,6 @@ vim.o.cursorline = true
 vim.o.dir = os.getenv('HOME') .. '/.vim/tmp'
 vim.o.encoding = 'utf8'
 vim.o.expandtab = true
-vim.o.grepformat = '%f:%l:%c:%m,%f:%l:%m'
-vim.o.grepprg = 'rg --vimgrep --smart-case'
 vim.o.hidden = true
 vim.o.hlsearch = false
 vim.o.incsearch = true
@@ -133,32 +182,12 @@ vim.opt.wildignore:append "**/node_modules/*"
 vim.opt.wildignorecase = true
 
 -- Treesitter setup
-
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "vimdoc", "javascript", "typescript", "lua" },
-
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
+    ensure_installed = { "vimdoc", "javascript", "typescript", "tsx", "lua", "java" },
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = false,
-
-    -- List of parsers to ignore installing (for "all")
-    -- ignore_install = { "javascript" },
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
     highlight = {
         enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
 }
@@ -172,49 +201,80 @@ require("gruvbox").setup({
     contrast = "hard"
 })
 
+
 vim.cmd([[ hi! TermCursor guifg=NONE guibg=#ebdbb2 gui=NONE cterm=NONE ]])
 vim.cmd([[ hi! TermCursorNC guifg=#ebdbb2 guibg=#3c3836 gui=NONE cterm=NONE ]])
 
 -- keybindings --
-local telescope = require('telescope.builtin')
+local telescope_builtin = require('telescope.builtin')
 local default_opts = { remap = false }
-vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
+
+local wk = require("which-key")
+wk.add({
+    { "<leader>;",        ':',                          desc = "Run Command" },
+    { "<leader><leader>", telescope_builtin.find_files, desc = "Find Files" },
+    {
+        "<leader>S",
+        ':Telescope live_grep glob_pattern=!*{test,spec}.*<CR>',
+        desc =
+        "Search project (without tests)"
+    },
+    { "<leader>b",  group = "buffer" },
+    {
+        '<leader>bi',
+        ':Telescope buffers<CR>',
+        desc =
+        "List buffers"
+    },
+    {
+        '<leader>bS',
+        ':noa w<CR>',
+        desc =
+        "Save (without formatting)"
+    },
+    { '<leader>bs', ':w<CR>',                             desc = "Save" },
+    { "<leader>c",  ':copen<CR>',                         desc = "Open Quickfix" },
+    { "<leader>g",  group = "Git" },
+    { "<leader>gd", ':Neogit diff<CR>',                   desc = "Neogit Diff" },
+    { "<leader>gg", ':Neogit<CR>',                        desc = "Open Neogit" },
+    { "<leader>n",  ':Telescope neorg find_linkable<CR>', desc = "Find Neorg Heading" },
+    {
+        "<leader>s",
+        ':Telescope live_grep<CR>',
+        desc =
+        "Search project"
+    },
+    { "<leader>t",  group = "Neotest" },
+    { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "test file" },
+    {
+        "<leader>tt",
+        function() require("neotest").run.run() end,
+        desc =
+        "run test under cursor"
+    },
+    {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'npx jest --watch ' })<cr>",
+        desc =
+        "run test watch"
+    },
+    { "<leader>w", proxy = "<c-w>", group = "windows" },
+})
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('n', '<C-h>', '<C-w>h', default_opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', default_opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', default_opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', default_opts)
-vim.keymap.set('n', '<leader><space>', telescope.find_files, default_opts)
-vim.keymap.set('n', '<leader>B', ':bnext<CR>', default_opts)
-vim.keymap.set('n', '<leader>D', ':DapContinue<CR>', default_opts)
-vim.keymap.set('n', '<leader>S', ":grep -g '!**/**test*' -i ", default_opts)
-vim.keymap.set('n', '<leader>W', ':noa w<CR>', default_opts)
-vim.keymap.set('n', '<leader>b', ':bprevious<CR>', default_opts)
-vim.keymap.set('n', '<leader>c', ':copen<CR>', default_opts)
-vim.keymap.set('n', '<leader>d', ':Gvdiff<CR>', default_opts)
-vim.keymap.set('n', '<leader>s', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", default_opts)
-vim.keymap.set('n', '<leader>t', ':belowright sp | b zsh<CR>', default_opts)
-vim.keymap.set('n', '<leader>w', ':w<CR>', default_opts)
-vim.keymap.set('n', '<leader>h', '<C-w>h', default_opts)
-vim.keymap.set('n', '<leader>j', '<C-w>j', default_opts)
-vim.keymap.set('n', '<leader>k', '<C-w>k', default_opts)
-vim.keymap.set('n', '<leader>l', '<C-w>l', default_opts)
-vim.keymap.set('n', '<leader>H', '<C-w>H', default_opts)
-vim.keymap.set('n', '<leader>J', '<C-w>J', default_opts)
-vim.keymap.set('n', '<leader>K', '<C-w>K', default_opts)
-vim.keymap.set('n', '<leader>L', '<C-w>L', default_opts)
-vim.keymap.set('n', '<leader>wo', ':only<CR>', default_opts)
-vim.keymap.set('n', '<leader>ws', ':sp<CR>', default_opts)
-vim.keymap.set('n', '<leader>wv', ':vs<CR>', default_opts)
-vim.keymap.set('n', '<leader>;', telescope.commands, default_opts)
 vim.keymap.set('n', 'Y', '"+y', default_opts)
-vim.keymap.set('n', 'gD', ":lua require('dapui').toggle()<CR>", default_opts)
-vim.keymap.set('n', 'gb', ':DapToggleBreakpoint<CR>', default_opts)
 vim.keymap.set('t', '<C-[>', [[<C-\><C-n>]], default_opts)
 vim.keymap.set('t', 'jk', [[<C-\><C-n>]], default_opts)
 vim.keymap.set('v', 'Y', '"+y', default_opts)
 
 -- Autocommands --
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+    pattern = "*",
+    callback = function()
+        vim.diagnostic.open_float()
+    end
+})
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     callback = function()
         vim.lsp.buf.format()
@@ -240,6 +300,37 @@ vim.api.nvim_create_autocmd('TermOpen', {
     end
 })
 
+local insert_neorg_link = function()
+    local link = vim.fn.input("Link: ")
+    local text = vim.fn.input("Text: ")
+
+    vim.api.nvim_set_current_line("{" .. link .. "}[" .. text .. "]")
+end
+
+local insert_markdown_link = function()
+    local link = vim.fn.input("Link: ")
+    local text = vim.fn.input("Text: ")
+
+    vim.api.nvim_set_current_line("[" .. text .. "](" .. link .. ")")
+end
+
+
+vim.api.nvim_create_autocmd("Filetype", {
+    pattern = "norg",
+    callback = function()
+        vim.keymap.set("n", "<leader>t", "<Plug>(neorg.qol.todo-items.todo.task-cycle)", { buffer = true })
+        vim.keymap.set("i", "<C-l>", insert_neorg_link, { buffer = true })
+    end,
+})
+
+vim.api.nvim_create_autocmd("Filetype", {
+    pattern = "markdown",
+    callback = function()
+        vim.keymap.set("i", "<C-l>", insert_markdown_link, { buffer = true })
+    end,
+})
+
+
 -- LSP --
 -- Add additional LSP capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -247,7 +338,6 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- LSP SETUP --
 local nvim_lsp = require('lspconfig')
-
 -- This is run every time a language server is connected
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -255,16 +345,14 @@ local on_attach = function(client, bufnr)
     -- Mappings.
     local opts = { noremap = true, silent = true }
     buf_set_keymap('n', '<leader>a', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('v', '<leader>a', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
     buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'gh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'ge', '<Cmd>lua vim.diagnostic.open_float()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 end
 
--- Sets up all language servers (aside from tsserver and lua_ls)
+-- Sets up all language servers (aside from ts_ls and lua_ls)
 -- with the on_attach from above and the capabilities from nvim-cmp (completion)
 local servers = { "bashls", "cssls", "jsonls" }
 for _, lsp in ipairs(servers) do
@@ -274,9 +362,9 @@ for _, lsp in ipairs(servers) do
     }
 end
 
--- Sets up tsserver with the on_attach from above, the capabilities from nvim-cmp (completion),
+-- Sets up ts_ls with the on_attach from above, the capabilities from nvim-cmp (completion),
 -- a root directory that contains `.git`, and the ability to organize imports.
-nvim_lsp.tsserver.setup {
+nvim_lsp.ts_ls.setup {
     on_attach = function(client, bufnr)
         if client.config.flags then
             client.config.flags.allow_incremental_sync = true
@@ -371,91 +459,4 @@ if (cmp ~= nil) then
     ]]
 end
 
--- Debugging --
-local dap, dapui = require("dap"), require("dapui")
-dapui.setup()
-
-require("nvim-dap-virtual-text").setup()
-
-dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-end
-
-require("dap-vscode-js").setup({
-    adapters = { 'pwa-node' }
-})
-
-local js_filetypes = { 'javascript', 'javascript.jsx', 'typescript', 'typescript.tsx', 'cucumber' }
-for _, language in ipairs(js_filetypes) do
-    require("dap").configurations[language] = {
-        {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require 'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
-        },
-        {
-            type = "pwa-node",
-            request = "launch",
-            name = "Debug Jest Tests",
-            runtimeExecutable = "node",
-            runtimeArgs = {
-                "./node_modules/jest/bin/jest.js",
-                "--config",
-                "jest.ui.config.js",
-                "--setupFiles",
-                "dotenv/config"
-            },
-            rootPath = "${workspaceFolder}",
-            cwd = "${workspaceFolder}",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-        },
-        {
-            name = "Cucumber: @debug",
-            type = "pwa-node",
-            request = "launch",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-            env = {
-                BROWSER = "mobile"
-            },
-            runtimeArgs = {
-                "${workspaceFolder}/node_modules/@cucumber/cucumber/bin/cucumber-js",
-                "--tags",
-                "@debug",
-                "--config",
-                "./cucumber-debug.js"
-            },
-            cwd = "${workspaceFolder}"
-        }
-    }
-end
-
--- Telescope setup --
-local telescope = require("telescope")
-local lga_actions = require("telescope-live-grep-args.actions")
-
-telescope.setup {
-    defaults = { file_ignore_patterns = { "node_modules" } },
-    extensions = {
-        live_grep_args = {
-            auto_quoting = true, -- enable/disable auto-quoting
-            -- override default mappings
-            -- default_mappings = {},
-            mappings = { -- extend mappings
-                i = {
-                    ["<C-k>"] = lga_actions.quote_prompt({ postfix = [[ -g '!**/**test*' -i]] })
-
-                }
-            }
-        }
-    }
-}
+require("oil").setup()
