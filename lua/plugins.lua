0cb74ddc5c29fe651a746d30f0927cdd4957145b
Sam Gottfried <sgottfrd@gmail.com>
2025-01-22 10:48:02 -0500

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,16 +2,17 @@
 -- get lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
+vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
+
 
 require("lazy").setup({
     'APZelos/blamer.nvim',
@@ -32,53 +33,94 @@ require("lazy").setup({
     'tpope/vim-surround',
     'tpope/vim-unimpaired',
     'williamboman/mason.nvim',
-    { 'ahmedkhalf/project.nvim',
-        config = function()
-            require('telescope').load_extension('projects')
-            require('project_nvim').setup {}
-        end },
-    { 'glacambre/firenvim', build = ":call firenvim#install(0)" },
-    { "NeogitOrg/neogit",
-        cmd = "Neogit",
-        dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim", },
-        config = true },
-    { "folke/which-key.nvim", event = "VeryLazy",
-        keys = { { "<leader>?", function() require("which-key").show({ global = false }) end,
-            desc = "Buffer Local Keymaps (which-key)", }, }, },
     {
-        "nvim-neorg/neorg",
-        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme gruvbox")
+        require("gruvbox").setup({
+            contrast = "hard"
+            })
+      end,
+        set_light_mode = function()
+          vim.api.nvim_set_option_value("background", "light", {})
+          vim.cmd("colorscheme gruvbox")
+          end,
+    },
+    },
+    {
+      'ahmedkhalf/project.nvim',
+      config = function()
+        require('telescope').load_extension('projects')
+        require('project_nvim').setup {}
+      end
+    },
+    {
+      "3rd/image.nvim",
+      build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {}
+    },
+    { 'glacambre/firenvim',              build = ":call firenvim#install(0)" },
+    {
+      "NeogitOrg/neogit",
+      cmd = "Neogit",
+      dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim", },
+      config = true
+    },
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      keys = { {
+        "<leader>?",
+        function() require("which-key").show({ global = false }) end,
+        desc = "Buffer Local Keymaps (which-key)",
+      }, },
+    },
+    {
+      "nvim-neorg/neorg",
+      lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
         version = "*", -- Pin Neorg to the latest stable release
         opts = {
-            load = { ["core.defaults"] = {},
-                ["core.concealer"] = {},
-                ["core.integrations.treesitter"] = {},
-                ["core.integrations.telescope"] = {},
-                ["core.journal"] = { config = { workspace = "journal", } },
-                ["core.dirman"] = { config = { default_workspace = "journal", workspaces = { journal = "~/notes" }, }, },
-                ["core.qol.todo_items"] = {}, ["core.ui"] = {}, }
+          load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {},
+            ["core.integrations.treesitter"] = {},
+            ["core.integrations.telescope"] = {},
+            ["core.journal"] = { config = { workspace = "journal", } },
+            ["core.dirman"] = { config = { default_workspace = "journal", workspaces = { journal = "~/notes" }, }, },
+            ["core.qol.todo_items"] = {},
+            ["core.ui"] = {},
+          }
         },
-        dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
+      dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
     },
-    { "nvim-neotest/neotest",
-        dependencies = { "nvim-neotest/nvim-nio", "nvim-lua/plenary.nvim", "antoinemadec/FixCursorHold.nvim",
-            "nvim-treesitter/nvim-treesitter", "nvim-neotest/neotest-jest" },
-        config = function() require('neotest').setup({ adapters = { require('neotest-jest')({ cwd = function(path) return vim
-                    .fn.getcwd()
-            end, }), } })
-        end },
+    {
+      "nvim-neotest/neotest",
+      dependencies = { "nvim-neotest/nvim-nio", "nvim-lua/plenary.nvim", "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter", "nvim-neotest/neotest-jest" },
+      config = function()
+        require('neotest').setup({
+            adapters = { require('neotest-jest')({
+                cwd = function(path)
+                return vim
+                .fn.getcwd()
+                end,
+                }), }
+            })
+      end
+    },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", },
-    { "vhyrro/luarocks.nvim", priority = 1000, config = true },
+    { "vhyrro/luarocks.nvim",            priority = 1000,                    config = true },
     { 'creativenull/efmls-configs-nvim', version = 'v1.x.x', },
-    { 'nvim-lualine/lualine.nvim', dependencies = { { 'kyazdani42/nvim-web-devicons', opt = true } },
-        config = function() require('lualine').setup({ options = { theme = 'gruvbox', },
-                sections = { lualine_c = { "filename",
-                    function() return require("nvim-treesitter").statusline({ indicator_size = 100,
-                            type_patterns = { "class", "function", "method", "pair" }, separator = " -> ", })
-                    end, },
-                    lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" }, }, })
-        end },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.3', cmd = "Telescope" },
-    { 'stevearc/oil.nvim', dependencies = { "nvim-tree/nvim-web-devicons" }, },
-    { 'windwp/nvim-autopairs', event = "InsertEnter", opts = {} },
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { { 'kyazdani42/nvim-web-devicons', opt = true } },
+      config = function()
+        end
+    },
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.3',                                    cmd = "Telescope" },
+    { 'stevearc/oil.nvim',             dependencies = { "nvim-tree/nvim-web-devicons" }, },
+    { 'windwp/nvim-autopairs',         event = "InsertEnter",                            opts = {} },
 })
