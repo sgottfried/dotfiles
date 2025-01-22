082ac56f5cf6db6e24ccccf038ff3bb3236b5e8c
Sam Gottfried <sgottfrd@gmail.com>
2025-01-22 13:46:25 -0500

    Adds catpuccin theme.

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,17 +1,17 @@
 vim.cmd([[ hi! TermCursor guifg=NONE guibg=#ebdbb2 gui=NONE cterm=NONE ]])
 vim.cmd([[ hi! TermCursorNC guifg=#ebdbb2 guibg=#3c3836 gui=NONE cterm=NONE ]])
 
-  require('lualine').setup({
-      options = { theme = 'gruvbox', },
-      sections = {
-      lualine_c = { "filename",
-      function()
-      return require("nvim-treesitter").statusline({
-          indicator_size = 100,
-          type_patterns = { "class", "function", "method", "pair" },
-          separator = " -> ",
-          })
-      end, },
-      lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
-      },
-      })
+require('lualine').setup({
+    options = { theme = 'catpuccin', },
+    sections = {
+        lualine_c = { "filename",
+            function()
+                return require("nvim-treesitter").statusline({
+                    indicator_size = 100,
+                    type_patterns = { "class", "function", "method", "pair" },
+                    separator = " -> ",
+                })
+            end, },
+        lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
+    },
+})
