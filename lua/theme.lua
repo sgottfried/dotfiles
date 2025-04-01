dc8bca0a979293eb99662e64ccc0194cc5f6e1cc
Sam Gottfried <sgottfrd@gmail.com>
2025-04-01 16:27:17 -0400

    Adds gruvbox dimming.

--- lua/theme.lua
+++ lua/theme.lua
@@ -2,7 +2,7 @@ vim.cmd([[ hi! TermCursor guifg=NONE guibg=#ebdbb2 gui=NONE cterm=NONE ]])
 vim.cmd([[ hi! TermCursorNC guifg=#ebdbb2 guibg=#3c3836 gui=NONE cterm=NONE ]])
 
 -- vim.api.nvim_set_option_value("background", "dark", {})
--- require('gruvbox').setup({ contrast = "hard" })
+require('gruvbox').setup({ dim_inactive = true })
 -- vim.cmd("colorscheme gruvbox")
 require('lualine').setup({
     options = { theme = 'gruvbox', },
