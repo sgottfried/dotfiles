46ee40def3d89c2ed20b3c3d38875df1abc1cc82
Sam Gottfried <sgottfrd@gmail.com>
2025-06-30 09:03:41 -0400

    Adds octo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -205,4 +205,18 @@ require("lazy").setup({
     },
     { 'stevearc/oil.nvim', dependencies = { "nvim-tree/nvim-web-devicons" }, },
     { 'windwp/nvim-autopairs', event = "InsertEnter", opts = {} },
+    {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'folke/snacks.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require "octo".setup({
+                github_hostname = "github.ford.com",
+                picker = "snacks",
+            })
+        end
+    }
 })
