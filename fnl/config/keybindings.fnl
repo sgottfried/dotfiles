3aa53a7dac9ed5ab969da7b475adef8c7edc6489
Sam Gottfried <sgottfrd@gmail.com>
2025-09-03 11:38:25 -0400

    Adds macro for adding a group of keybindings for which-key.

--- fnl/config/keybindings.fnl
+++ fnl/config/keybindings.fnl
@@ -1,59 +1,68 @@
 (local default-opts {:remap false})
 (local wk (require :which-key))
 
-(macro add-group [keybinding group-name]
-         `(wk.add [{1 ,keybinding :group ,group-name}]))
+(macro add-group [prefix group-name mappings]
+  (do
+    `(wk.add [{1 ,prefix :group ,group-name}])
+    `(wk.add (icollect [_# [keybinding# command# desc#] (ipairs ,mappings)]
+               {1 (.. ,prefix keybinding#) 2 command# :desc desc#}))))
 
 (macro add-insert-mode-keybinding [keybinding command]
-         `(vim.keymap.set :i ,keybinding ,command))
+  `(vim.keymap.set :i ,keybinding ,command))
 
 (macro add-keybinding [keybinding command desc]
-         `(wk.add [{1 ,keybinding 2 ,command :desc ,desc}]))
+  `(wk.add [{1 ,keybinding 2 ,command :desc ,desc}]))
 
 (macro add-proxy-group [keybinding group-name proxy-keybinding]
-         `(wk.add [{1 ,keybinding :group ,group-name :proxy ,proxy-keybinding}]))
+  `(wk.add [{1 ,keybinding :group ,group-name :proxy ,proxy-keybinding}]))
 
 (macro add-terminal-mode-keybinding [keybinding command]
-         `(vim.keymap.set :t ,keybinding ,command default-opts))
+  `(vim.keymap.set :t ,keybinding ,command default-opts))
 
 (macro add-visual-mode-keybinding [keybinding command]
-         `(vim.keymap.set :v ,keybinding ,command default-opts))
+  `(vim.keymap.set :v ,keybinding ,command default-opts))
 
-(add-group :<leader>b :buffer)
-(add-group :<leader>d :debugger)
-(add-group :<leader>g :Git)
-(add-group :<leader>t :Neotest)
+(add-group :<leader>b :buffer [
+                               [:S ":noa w<CR>" "Save (without formatting)"]
+                               [:i ":Telescope buffers<CR>" "List buffers"]
+                               [:s ":w<CR>" "Save"]
+                               ])
+(add-group :<leader>d :debugger [
+                                 [:d ":DapContinue<CR>" "Continue Debugging"]
+                                 [:i ":DapStepInto<CR>" "Step Into"]
+                                 [:o ":DapStepOver<CR>" "Step Over"]
+                                 ])
+(add-group :<leader>g :Git [
+                            [:D ":Gvdiffsplit!<CR>" "Git Merge"]
+                            [:d ":Gvdiffsplit<CR>" "Git Diff"]
+                            [:g ":G<CR>" "Open Fugitive"]
+                            ])
+(add-group :<leader>n :neorg [
+                              [:m ":MigrateYesterdayTasks<CR>" "Neorg journal migrate tasks" ]
+                             [:j ":Neorg journal today<CR>" "Neorg journal for today"]
+                             ])
+(add-group :<leader>t :Neotest [
+                                [:d (fn []
+                                      ((. (require :neotest) :run :run) {:strategy :dap})) "Debug Test"]
+                                [:f (fn []
+                                      ((. (require :neotest) :run :run) (vim.fn.expand "%"))) "Test File"]
+                                [:t (fn []
+                                      ((. (require :neotest) :run :run) {:jestCommand "npx jest --coverage"})) "Run Test Under Cursor"]
+                                [:w "<cmd>lua require('neotest').run.run({ jestCommand = 'npx jest --watch ' })<cr>" "Run Test in Watch Mode"]
+                                ])
 (add-insert-mode-keybinding :jk :<Esc>)
 (add-keybinding "-" ":Oil<CR>" "Open parent directory")
 (add-keybinding "<leader>;" ":" :desc "Run Command")
 (add-keybinding :<leader><leader> ":Telescope find_files<CR>" "Telescope find files")
-(add-keybinding :<leader>bS ":noa w<CR>" "Save (without formatting)")
-(add-keybinding :<leader>bi ":Telescope buffers<CR>" "List buffers")
-(add-keybinding :<leader>bs ":w<CR>" "Save")
 (add-keybinding :<leader>c ":copen<CR>" "Open Quickfix")
-(add-keybinding :<leader>dd ":DapContinue<CR>" "Continue Debugging")
-(add-keybinding :<leader>di ":DapStepInto<CR>" "Step Into")
-(add-keybinding :<leader>do ":DapStepOver<CR>" "Step Over")
-(add-keybinding :<leader>gD ":Gvdiffsplit!<CR>" "Fugitive Merge")
-(add-keybinding :<leader>gd ":Gvdiffsplit<CR>" "Fugitive Diff")
-(add-keybinding :<leader>gg ":G<CR>" "Open Fugitive")
 (add-keybinding :<leader>hh ":Telescope help_tags<CR>" "Search Helptags")
-(add-keybinding :<leader>nj ":Neorg journal today<CR>" "Neorg journal for today")
-(add-keybinding :<leader>nm ":MigrateYesterdayTasks<CR>" "Neorg journal migrate tasks")
 (add-keybinding :<leader>ot (fn []
-                             (let [term-buf (vim.fn.bufnr "term://*")]
-                               (if (= term-buf (- 1))
-                                   (vim.cmd "botright split | resize 20 | terminal")
-                                   (vim.cmd (.. "botright split | resize 20 | buffer "
-                                                term-buf))))) "Open Terminal")
+                              (let [term-buf (vim.fn.bufnr "term://*")]
+                                (if (= term-buf (- 1))
+                                    (vim.cmd "botright split | resize 20 | terminal")
+                                    (vim.cmd (.. "botright split | resize 20 | buffer "
+                                                 term-buf))))) "Open Terminal")
 (add-keybinding :<leader>s ":Telescope live_grep<CR>" "Search project")
-(add-keybinding :<leader>td (fn []
-                             ((. (require :neotest) :run :run) {:strategy :dap})) "debug test")
-(add-keybinding :<leader>tt (fn []
-                             ((. (require :neotest) :run :run) {:jestCommand "npx jest --coverage"})) "run test under cursor")
-(add-keybinding :<leader>tf (fn []
-                             ((. (require :neotest) :run :run) (vim.fn.expand "%"))) "test file")
-(add-keybinding :<leader>tw "<cmd>lua require('neotest').run.run({ jestCommand = 'npx jest --watch ' })<cr>" "run test watch")
 (add-keybinding :<leader>x ":.lua<CR>" "Execute Lua line")
 (add-keybinding :gb ":DapToggleBreakpoint<CR>" "toggle breakpoint")
 (add-keybinding :Y "\"+y" "Yank to system clipboard")
--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -1,28 +1,107 @@
 -- [nfnl] fnl/config/keybindings.fnl
 local default_opts = {remap = false}
 local wk = require("which-key")
-wk.add({{"<leader>b", group = "buffer"}})
-wk.add({{"<leader>d", group = "debugger"}})
-wk.add({{"<leader>g", group = "Git"}})
-wk.add({{"<leader>t", group = "Neotest"}})
+local function _1_(...)
+  local tbl_21_ = {}
+  local i_22_ = 0
+  for __2_auto, _2_ in ipairs({{"S", ":noa w<CR>", "Save (without formatting)"}, {"i", ":Telescope buffers<CR>", "List buffers"}, {"s", ":w<CR>", "Save"}}) do
+    local keybinding_3_auto = _2_[1]
+    local command_4_auto = _2_[2]
+    local desc_5_auto = _2_[3]
+    local val_23_ = {("<leader>b" .. keybinding_3_auto), command_4_auto, desc = desc_5_auto}
+    if (nil ~= val_23_) then
+      i_22_ = (i_22_ + 1)
+      tbl_21_[i_22_] = val_23_
+    else
+    end
+  end
+  return tbl_21_
+end
+wk.add(_1_(...))
+local function _4_(...)
+  local tbl_21_ = {}
+  local i_22_ = 0
+  for __2_auto, _5_ in ipairs({{"d", ":DapContinue<CR>", "Continue Debugging"}, {"i", ":DapStepInto<CR>", "Step Into"}, {"o", ":DapStepOver<CR>", "Step Over"}}) do
+    local keybinding_3_auto = _5_[1]
+    local command_4_auto = _5_[2]
+    local desc_5_auto = _5_[3]
+    local val_23_ = {("<leader>d" .. keybinding_3_auto), command_4_auto, desc = desc_5_auto}
+    if (nil ~= val_23_) then
+      i_22_ = (i_22_ + 1)
+      tbl_21_[i_22_] = val_23_
+    else
+    end
+  end
+  return tbl_21_
+end
+wk.add(_4_(...))
+local function _7_(...)
+  local tbl_21_ = {}
+  local i_22_ = 0
+  for __2_auto, _8_ in ipairs({{"D", ":Gvdiffsplit!<CR>", "Git Merge"}, {"d", ":Gvdiffsplit<CR>", "Git Diff"}, {"g", ":G<CR>", "Open Fugitive"}}) do
+    local keybinding_3_auto = _8_[1]
+    local command_4_auto = _8_[2]
+    local desc_5_auto = _8_[3]
+    local val_23_ = {("<leader>g" .. keybinding_3_auto), command_4_auto, desc = desc_5_auto}
+    if (nil ~= val_23_) then
+      i_22_ = (i_22_ + 1)
+      tbl_21_[i_22_] = val_23_
+    else
+    end
+  end
+  return tbl_21_
+end
+wk.add(_7_(...))
+local function _10_(...)
+  local tbl_21_ = {}
+  local i_22_ = 0
+  for __2_auto, _11_ in ipairs({{"m", ":MigrateYesterdayTasks<CR>", "Neorg journal migrate tasks"}, {"j", ":Neorg journal today<CR>", "Neorg journal for today"}}) do
+    local keybinding_3_auto = _11_[1]
+    local command_4_auto = _11_[2]
+    local desc_5_auto = _11_[3]
+    local val_23_ = {("<leader>n" .. keybinding_3_auto), command_4_auto, desc = desc_5_auto}
+    if (nil ~= val_23_) then
+      i_22_ = (i_22_ + 1)
+      tbl_21_[i_22_] = val_23_
+    else
+    end
+  end
+  return tbl_21_
+end
+wk.add(_10_(...))
+local function _13_(...)
+  local tbl_21_ = {}
+  local i_22_ = 0
+  local function _15_()
+    return require("neotest").run.run({strategy = "dap"})
+  end
+  local function _16_()
+    return require("neotest").run.run(vim.fn.expand("%"))
+  end
+  local function _17_()
+    return require("neotest").run.run({jestCommand = "npx jest --coverage"})
+  end
+  for __2_auto, _14_ in ipairs({{"d", _15_, "Debug Test"}, {"f", _16_, "Test File"}, {"t", _17_, "Run Test Under Cursor"}, {"w", "<cmd>lua require('neotest').run.run({ jestCommand = 'npx jest --watch ' })<cr>", "Run Test in Watch Mode"}}) do
+    local keybinding_3_auto = _14_[1]
+    local command_4_auto = _14_[2]
+    local desc_5_auto = _14_[3]
+    local val_23_ = {("<leader>t" .. keybinding_3_auto), command_4_auto, desc = desc_5_auto}
+    if (nil ~= val_23_) then
+      i_22_ = (i_22_ + 1)
+      tbl_21_[i_22_] = val_23_
+    else
+    end
+  end
+  return tbl_21_
+end
+wk.add(_13_(...))
 vim.keymap.set("i", "jk", "<Esc>")
 wk.add({{"-", ":Oil<CR>", desc = "Open parent directory"}})
 wk.add({{"<leader>;", ":", desc = "desc"}})
 wk.add({{"<leader><leader>", ":Telescope find_files<CR>", desc = "Telescope find files"}})
-wk.add({{"<leader>bS", ":noa w<CR>", desc = "Save (without formatting)"}})
-wk.add({{"<leader>bi", ":Telescope buffers<CR>", desc = "List buffers"}})
-wk.add({{"<leader>bs", ":w<CR>", desc = "Save"}})
 wk.add({{"<leader>c", ":copen<CR>", desc = "Open Quickfix"}})
-wk.add({{"<leader>dd", ":DapContinue<CR>", desc = "Continue Debugging"}})
-wk.add({{"<leader>di", ":DapStepInto<CR>", desc = "Step Into"}})
-wk.add({{"<leader>do", ":DapStepOver<CR>", desc = "Step Over"}})
-wk.add({{"<leader>gD", ":Gvdiffsplit!<CR>", desc = "Fugitive Merge"}})
-wk.add({{"<leader>gd", ":Gvdiffsplit<CR>", desc = "Fugitive Diff"}})
-wk.add({{"<leader>gg", ":G<CR>", desc = "Open Fugitive"}})
 wk.add({{"<leader>hh", ":Telescope help_tags<CR>", desc = "Search Helptags"}})
-wk.add({{"<leader>nj", ":Neorg journal today<CR>", desc = "Neorg journal for today"}})
-wk.add({{"<leader>nm", ":MigrateYesterdayTasks<CR>", desc = "Neorg journal migrate tasks"}})
-local function _1_()
+local function _19_()
   local term_buf = vim.fn.bufnr("term://*")
   if (term_buf == ( - 1)) then
     return vim.cmd("botright split | resize 20 | terminal")
@@ -30,21 +109,8 @@ local function _1_()
     return vim.cmd(("botright split | resize 20 | buffer " .. term_buf))
   end
 end
-wk.add({{"<leader>ot", _1_, desc = "Open Terminal"}})
+wk.add({{"<leader>ot", _19_, desc = "Open Terminal"}})
 wk.add({{"<leader>s", ":Telescope live_grep<CR>", desc = "Search project"}})
-local function _3_()
-  return require("neotest").run.run({strategy = "dap"})
-end
-wk.add({{"<leader>td", _3_, desc = "debug test"}})
-local function _4_()
-  return require("neotest").run.run({jestCommand = "npx jest --coverage"})
-end
-wk.add({{"<leader>tt", _4_, desc = "run test under cursor"}})
-local function _5_()
-  return require("neotest").run.run(vim.fn.expand("%"))
-end
-wk.add({{"<leader>tf", _5_, desc = "test file"}})
-wk.add({{"<leader>tw", "<cmd>lua require('neotest').run.run({ jestCommand = 'npx jest --watch ' })<cr>", desc = "run test watch"}})
 wk.add({{"<leader>x", ":.lua<CR>", desc = "Execute Lua line"}})
 wk.add({{"gb", ":DapToggleBreakpoint<CR>", desc = "toggle breakpoint"}})
 wk.add({{"Y", "\"+y", desc = "Yank to system clipboard"}})
