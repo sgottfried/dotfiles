bc27103b27522e513e7fd27f5aabb7ac5ca74bed
Sam Gottfried <sgottfrd@gmail.com>
2025-11-05 09:47:10 -0500

    Add initial Doom Emacs configuration
    This configuration file sets up various settings for Doom Emacs, including font settings, theme, line numbers, key bindings, and package configurations.

---
+++ doom/config.el
@@ -0,0 +1,145 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'semi-light))
+;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-gruvbox-variant 'dark-medium)
+(setq doom-theme 'doom-gruvbox)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word))
+  :defer t)
+(use-package copilot-chat
+  :bind (:map global-map
+              ("C-c C-y" . copilot-chat-yank)
+              ("C-c M-y" . copilot-chat-yank-pop)
+              ("C-c C-M-y" . (lambda () (interactive) (copilot-chat-yank-pop -1))))
+  :defer t
+  )
+(use-package! blamer :defer t :config (blamer-mode 1))
+
+(setq flycheck-check-syntax-automatically '(save mode-enable))
+
+(evil-define-key 'normal 'global (kbd "C-w o") 'delete-other-windows)
+(evil-define-key 'normal 'global (kbd "C-h") 'evil-window-left)
+(evil-define-key 'normal 'global (kbd "C-j") 'evil-window-down)
+(evil-define-key 'normal 'global (kbd "C-k") 'evil-window-up)
+(evil-define-key 'normal 'global (kbd "C-l") 'evil-window-right)
+(evil-define-key 'normal 'global (kbd "-") 'dired-jump)
+(evil-define-key 'normal 'global (kbd "gh") 'lsp-ui-doc-glance)
+(evil-define-key 'normal 'global (kbd ",d") 'magit-ediff-show-unstaged)
+(evil-define-key 'normal 'global (kbd ",l") 'doom/toggle-line-numbers)
+(evil-define-key 'normal 'global (kbd "s-{") '+workspace/switch-left)
+(evil-define-key 'normal 'global (kbd "s-}") '+workspace/switch-right)
+(map! :leader
+      :desc "Toggle Alternate File"
+      "f a" #'projectile-toggle-between-implementation-and-test)
+(map! :leader
+      :desc "Git diff unstaged"
+      "g d" #'magit-ediff-show-unstaged)
+
+(map! :leader
+      :desc "Close other windows"
+      "w o" #'delete-other-windows)
+(global-set-key (kbd "C-{") 'vterm-copy-mode)
+
+;; Org-mode
+(setq org-agenda-files (directory-files-recursively "~/notes" "\\.org$"))
+(after! org
+  (setq evil-auto-indent nil))
+(setq electric-indent-mode nil)
+(setq org-src-preserve-indentation t)
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(require 'tree-sitter-langs)
+
+;; performance optimizations
+(setq gc-cons-threshold (* 64 1024 1024)) ; 64MB during startup
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 16 1024 1024)))) ; 16MB after startup
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t)
+
+(setq native-comp-jit-compilation t)
