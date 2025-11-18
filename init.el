9195f21c8cc1aaa40cec78235b50737bbff0a918
Sam Gottfried <sgottfrd@gmail.com>
2025-11-18 09:30:33 -0500

    Moves package management to top.

--- init.el
+++ init.el
@@ -1,5 +1,23 @@
 ;; Generated from init.org
 
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+(use-package diminish :ensure t)
+
+(use-package diminish :ensure t)
+
 (use-package doom-themes
   :init (load-theme 'doom-gruvbox t))
 
@@ -92,24 +110,6 @@
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Initialize package sources
-(require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-(use-package diminish :ensure t)
-
-(use-package diminish :ensure t)
 
 (use-package ivy
 :diminish
