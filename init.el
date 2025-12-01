a920127e0cec16e767f2a89ffcec77a5a27f8d1d
Sam Gottfried <sgottfrd@gmail.com>
2025-12-01 11:01:57 -0500

    feat(emacs): Add evil-org package with agenda integration
    This commit introduces the `evil-org` package to enhance the Org
    mode experience with Evil keybindings. It includes integration with
    the `evil-org-agenda` module and configures necessary hooks for
    enabling `evil-org-mode` in Org mode.

--- init.el
+++ init.el
@@ -180,6 +180,14 @@
   :config
   (evil-collection-init))
 
+(use-package evil-org
+  :ensure t
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
 (use-package key-chord)
 (require 'key-chord)
 (key-chord-mode 1)
@@ -498,3 +506,23 @@
 
 (use-package vterm
     :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(blamer command-log-mode company-box copilot counsel-projectile
+	    diminish doom-modeline doom-themes evil-collection
+	    evil-commentary evil-org feature-mode flycheck forge
+	    general git-gutter-fringe gptel-magit helpful hydra
+	    indent-bars ivy-rich json-mode key-chord lsp-ivy lsp-ui
+	    org-auto-tangle org-bullets org-gtd org-roam perspective
+	    prettier-js rainbow-delimiters tree-sitter-langs
+	    typescript-mode visual-fill visual-fill-column vterm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
