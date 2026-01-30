e1ebc4dd989fdc0217300e2a31a45729845f09f8
Sam Gottfried <sgottfrd@gmail.com>
2026-01-30 10:25:34 -0500

    Change color scheme and update key bindings

--- wezterm.lua
+++ wezterm.lua
@@ -4,9 +4,9 @@ local mux = wezterm.mux
 
 local function scheme_for_appearance(appearance)
   if string.find(appearance, 'Dark') then
-    return "Gruvbox dark, hard (base16)"
+    return "Catppuccin Mocha"
   else
-    return "Gruvbox light, hard (base16)"
+    return "Catppuccin Latte"
   end
 end
 
@@ -37,32 +37,42 @@ return {
       key = "v",
       mods = "LEADER",
     },
-    { action = act.AdjustPaneSize({ "Left", 5 }), key = "H", mods = "LEADER" },
+    { action = act.AdjustPaneSize({ "Left", 5 }),  key = "H", mods = "LEADER" },
     { action = act.AdjustPaneSize({ "Right", 5 }), key = "L", mods = "LEADER" },
-    { action = act.AdjustPaneSize({ "Down", 5 }), key = "J", mods = "LEADER" },
-    { action = act.AdjustPaneSize({ "Up", 5 }), key = "K", mods = "LEADER" },
-    { action = act.SpawnTab("CurrentPaneDomain"), key = "c", mods = "LEADER" },
-    { action = act.ActivatePaneDirection("Left"), key = "h", mods = "LEADER" },
-    { action = act.ActivatePaneDirection("Down"), key = "j", mods = "LEADER" },
-    { action = act.ActivatePaneDirection("Up"), key = "k", mods = "LEADER" },
+    { action = act.AdjustPaneSize({ "Down", 5 }),  key = "J", mods = "LEADER" },
+    { action = act.AdjustPaneSize({ "Up", 5 }),    key = "K", mods = "LEADER" },
+    { action = act.SpawnTab("CurrentPaneDomain"),  key = "c", mods = "LEADER" },
+    { action = act.ActivatePaneDirection("Left"),  key = "h", mods = "LEADER" },
+    { action = act.ActivatePaneDirection("Down"),  key = "j", mods = "LEADER" },
+    { action = act.ActivatePaneDirection("Up"),    key = "k", mods = "LEADER" },
     { action = act.ActivatePaneDirection("Right"), key = "l", mods = "LEADER" },
-    { action = act.ActivateTabRelative(1), key = "n", mods = "LEADER" },
-    { action = act.ActivateTabRelative(-1), key = "p", mods = "LEADER" },
-    { action = act.ActivateCopyMode, key = "[", mods = "LEADER" },
-    { action = act.TogglePaneZoomState, key = "z", mods = "LEADER" },
-    { key = "r", mods = "LEADER", action = act.PromptInputLine {
-      description = 'Enter new name for tab',
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    },
+    { action = act.ActivateTabRelative(1),         key = "n", mods = "LEADER" },
+    { action = act.ActivateTabRelative(-1),        key = "p", mods = "LEADER" },
+    { action = act.ActivateCopyMode,               key = "[", mods = "LEADER" },
+    { action = act.TogglePaneZoomState,            key = "z", mods = "LEADER" },
+    {
+      key = "r",
+      mods = "LEADER",
+      action = act.PromptInputLine {
+        description = 'Enter new name for tab',
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
     },
   },
   leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 },
-  native_macos_fullscreen_mode = true,
+  native_macos_fullscreen_mode = false,
   tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
   window_decorations = "RESIZE",
+  wezterm.on('update-right-status', function(window)
+    local date = wezterm.strftime '%Y-%m-%d %H:%M:%S'
+
+    window:set_right_status(wezterm.format {
+      { Text = date },
+    })
+  end)
 }
