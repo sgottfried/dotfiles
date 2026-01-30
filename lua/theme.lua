91bf1f5d7c7fc49c6e239fce9bb3b0f64a010242
Sam Gottfried <sgottfrd@gmail.com>
2026-01-30 10:24:48 -0500

    Change lualine theme from gruvbox to catppuccin

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,14 +1,7 @@
--- [nfnl] fnl/config/theme.fnl
-vim.cmd("colorscheme gruvbox")
 vim.cmd(" hi! TermCursor guifg=NONE guibg=#ebdbb2 gui=NONE cterm=NONE ")
 vim.cmd(" hi! TermCursorNC guifg=#ebdbb2 guibg=#3c3836 gui=NONE cterm=NONE ")
-local function _1_()
-  return require("nvim-treesitter").statusline({ indicator_size = 100, separator = " -> ",
-    type_patterns = { "class", "function", "method", "pair" } })
-end
-
 require("lualine").setup({
-  options = { theme = "gruvbox" },
+  options = { theme = "catppuccin" },
   sections = {
     lualine_c = {
       "filename",
