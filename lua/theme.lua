1c9414014e9b03cc8d32dd4a53e94b4c3e3cc99e
Sam Gottfried <sgottfrd@gmail.com>
2026-04-01 08:37:27 -0400

    Change colorscheme to gruvbox-material

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,17 +1,24 @@
 vim.cmd(" hi! TermCursor guifg=NONE guibg=#ebdbb2 gui=NONE cterm=NONE ")
 vim.cmd(" hi! TermCursorNC guifg=#ebdbb2 guibg=#3c3836 gui=NONE cterm=NONE ")
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme gruvbox-material")
+
 require("lualine").setup({
-  options = { theme = "gruvbox" },
+  options = {
+    theme = "gruvbox-material"
+  },
   sections = {
     lualine_c = {
       "filename",
       function()
-        require("nvim-treesitter").statusline({
-          indicator_size = 100,
-          separator = " -> ",
-          type_patterns = { "class", "function", "method", "pair" }
-        })
+        local ts = require("nvim-treesitter")
+        if ts and ts.statusline then
+          return ts.statusline({
+            indicator_size = 100,
+            separator = " -> ",
+            type_patterns = { "class", "function", "method", "pair" }
+          })
+        end
+        return ""
       end
     },
     lualine_x = { "encoding", { "fileformat", symbols = { unix = "\238\156\145" } }, "filetype" }
