4abd10896e66ec7c686de5f5951d61b79374b915
Sam Gottfried <sgottfrd@gmail.com>
2026-04-01 08:36:53 -0400

    Refactor plugin configuration for Neovim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,197 +1,63 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local chat_model = "claude-3.5-sonnet"
+vim.pack.add({
+  -- Core/UI
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-lualine/lualine.nvim',
+  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/folke/which-key.nvim',
+  'https://github.com/sainnhe/gruvbox-material',
 
-if os.getenv("NEOVIM_ENVIRONMENT") == "work" then
-  chat_model = "claude-sonnet-4"
-end
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  -- {
-  --   dir = "~/workspace/nvim-plugins/stackmap.nvim"
-  -- },
-  -- -- Lua
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-      end,
-      update_interval = 3000,
-      fallback = "dark"
-    }
-  },
-  { 'nvim-lua/plenary.nvim', cmd = "PlenaryBustedFile" },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    config = function() end,
-    dependencies = { "nvim-web-devicons" }
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)"
-      }
-    }
-  },
-  -- Git integration
-  {
-    "airblade/vim-gitgutter",
-    event = { "BufRead", "BufNewFile" }
-  },
-  {
-    "APZelos/blamer.nvim",
-    event = { "BufRead", "BufNewFile" },
-    cmd = "BlamerToggle"
-  },
+  -- Git
+  'https://github.com/airblade/vim-gitgutter',
+  'https://github.com/APZelos/blamer.nvim',
 
   -- LSP and Completion
-  {
-    "neovim/nvim-lspconfig",
-    event = "BufReadPre"
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" }
-  },
-  {
-    "creativenull/efmls-configs-nvim",
-    version = "v1.x.x",
-    event = "BufReadPre"
-  },
-  -- Language and Syntax
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    event = { "BufReadPost", "BufNewFile" }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    build = ":TSUpdate"
-  },
-  {
-    "ianks/vim-tsx",
-    ft = { "typescript.tsx", "javascript.jsx" }
-  },
-  {
-    "leafgarland/typescript-vim",
-    ft = { "typescript", "javascript" }
-  },
-  {
-    "pangloss/vim-javascript",
-    ft = "javascript"
-  },
-  -- Editor Enhancement
-  {
-    "tpope/vim-commentary",
-    event = { "BufRead", "BufNewFile" }
-  },
-  {
-    "tpope/vim-surround",
-    event = { "BufRead", "BufNewFile" }
-  },
-  {
-    "tpope/vim-unimpaired",
-    event = "VeryLazy"
-  },
-  {
-    "tpope/vim-projectionist",
-    event = "VeryLazy"
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {}
-  },
-  {
-    "stevearc/oil.nvim",
-    cmd = "Oil",
-    dependencies = { "nvim-web-devicons" }
-  },
-  -- AI Integration
-  {
-    "github/copilot.vim",
-    event = "InsertEnter"
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    build = "make tiktoken",
-    config = function()
-      require("CopilotChat").setup({ model = chat_model })
-    end,
-    cmd = "CopilotChat",
-    dependencies = { "github/copilot.vim", "plenary.nvim" }
-  },
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/hrsh7th/nvim-cmp',
+  'https://github.com/hrsh7th/cmp-nvim-lsp',
+  'https://github.com/creativenull/efmls-configs-nvim',
+  'https://github.com/mfussenegger/nvim-lint',
 
-  -- Notes
-  {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*", -- use latest release, remove to use latest commit
-    ---@module 'obsidian'
-    ---@type obsidian.config
-    opts = {
-      legacy_commands = false, -- this will be removed in the next major release
-      daily_notes = {
-        template = "daily.md"
-      },
-      workspaces = {
-        {
-          name = "Sam's Notes",
-          path = "~/notes/obsidian",
-        },
-      },
-      templates = {
-        folder = "templates",
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
-      },
-      checkbox = {
-        enabled = true,
-        create_new = true,
-        order = { " ", ">", "x" },
-      },
-    }
+  -- Syntax
+  'https://github.com/HiPhish/rainbow-delimiters.nvim',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
 
-  },
+  -- Editor
+  'https://github.com/tpope/vim-commentary',
+  'https://github.com/tpope/vim-surround',
+  'https://github.com/tpope/vim-unimpaired',
+  'https://github.com/tpope/vim-projectionist',
+  'https://github.com/windwp/nvim-autopairs',
+  'https://github.com/stevearc/oil.nvim',
+
+  -- AI
+  'https://github.com/github/copilot.vim',
 
   -- Misc
-  {
-    "folke/snacks.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      image = {},
-      picker = {
-        layout = { preview = false, preset = "ivy_split" }
-      }
-    },
+  'https://github.com/folke/snacks.nvim',
+})
+
+require('oil').setup({
+  columns = {
+    "icon",
+    "size",
+    "mtime",
+    "permissions"
   }
 })
+
+-- Plugin configurations
+require('nvim-autopairs').setup({})
+
+require('snacks').setup({
+  image = {},
+  picker = {
+    layout = { preview = false, preset = "ivy_split" }
+  }
+})
+
+require('lint').linters_by_ft = {
+  javascript = { 'eslint' },
+  typescript = { 'eslint' },
+  javascriptreact = { 'eslint' },
+  typescriptreact = { 'eslint' },
+}
